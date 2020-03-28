2629d2d8923dacff81fbe08e153207905589838c
Alan Lo <lo.alan@gmail.com>
2020-03-28 18:37:29 +1100

    ref for levels

---
+++ 01_basic_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+import logging
+
+### Levels
+### Most info
+
+#logging.basicConfig(level=logging.NOTSET)
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.WARNING)
+#logging.basicConfig(level=logging.ERROR)
+#logging.basicConfig(level=logging.CRITICAL)
+
+### Least info
+
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# python logging
+
+Just a test repo with code to remind myself how python logging works because it drives me mad.
+
+Ref:
+ * https://www.machinelearningplus.com/python/python-logging-guide/
